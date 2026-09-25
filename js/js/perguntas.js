47f4ec99ebe351fd266534e756cf2390dc25d58b
danieletuard-web <daniele.tuard@escola.pr.gov.br>
2026-09-25 08:49:34 -0300

    Add perguntas.js with questions and alternatives

---
+++ js/js/perguntas.js
@@ -0,0 +1,40 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está advancing."
+                ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Foi atrás de vídeos, artigos e mais informações sobre como utilizar essa tecnologia."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, uma professora da escola resolveu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA na sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de IA para pesquisar dados e ajudar na elaboração do trabalho.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar fontes úteis para o trabalho.",
+                    "Percebeu que a IA pode ser uma grande aliada no aprendizado rápido."
+                ]
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas e pesquisas na biblioteca.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios conhecimentos e reflexões.",
+                    "Acredita que o contato humano e livros físicos ainda são essenciais."
+                ]
+            }
+        ]
+    }
+];
